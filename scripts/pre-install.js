23f9058b352db9e03101d1570a4299e8fceff841
siyuan.wang <siyuan.wang@capgemini.com>
2023-05-31 10:14:39 +0800

    feat: Changed pre-install checking text.

--- scripts/pre-install.js
+++ scripts/pre-install.js
@@ -1,7 +1,9 @@
-const nodeVersion = process.versions.node
-const execPath = process.env.npm_execpath
+const nodeVersion = process.versions.node;
+const execPath = process.env.npm_execpath;
 
-if (nodeVersion.localeCompare('16.15.1') < 0) {
-  console.log(`Please upgrade node To v16.15.1, current version is ${nodeVersion}`)
-  process.exit(1)
+if (nodeVersion.localeCompare("16.15.1") < 0) {
+  console.log(
+    `Please upgrade the NodeJS version to at least 16.15 or above, current version is ${nodeVersion}`
+  );
+  process.exit(1);
 }
